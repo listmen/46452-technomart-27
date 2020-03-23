db86c6b347b79657d0fbd173c75add3821d49158
listmen <48367007+listmen@users.noreply.github.com>
2020-03-23 22:13:33 +0300

    4. Личный проект: дополняем разметку

---
+++ js/script.js
@@ -0,0 +1,130 @@
+var link = document.querySelector(".login");
+var popup = document.querySelector(".login-popup");
+var close = document.querySelector(".login-popup-close");
+var login = document.querySelector(".login-field");
+var form = popup.querySelector("form");
+var login = form.querySelector(".login-field");
+var password = form.querySelector(".password-field");
+var storage = localStorage.getItem("login");
+/* slider */
+var ul;
+var li_items;
+var imageNumber;
+var imageWidth;
+var prev, next;
+var currentPostion = 0;
+var currentImage = 0;
+/* slider-2 */
+
+link.addEventListener("click", function () {
+  event.preventDefault();
+  popup.classList.add("login-popup-show");
+  if (storage) {
+    login.value = storage;
+    password.focus();
+  } else {
+    login.focus();
+  }
+}, false);
+
+close.addEventListener("click", function () {
+  event.preventDefault();
+  popup.classList.remove("login-popup-show");
+}, false);
+
+form.addEventListener("submit", function () {
+  if (!login.value || !password.value) {
+    event.preventDefault();
+    popup.classList.add("login-popup-error");
+  } else {
+    localStorage.setItem("login", login.value);
+  }
+}, false);
+
+window.addEventListener("keydown", function (event) {
+  if (event.keyCode == 27 && popup.classList.contains("login-popup-show")) {
+    popup.classList.remove("login-popup-show");
+  }
+}, false);
+
+/* slider */
+
+function init() {
+  ul = document.getElementById('slider');
+  li_items = ul.children;
+  imageNumber = li_items.length;
+  imageWidth = li_items[0].children[0].clientWidth;
+  ul.style.width = parseInt(imageWidth * imageNumber) + 'px';
+  prev = document.getElementById("prev");
+  next = document.getElementById("next");
+  //.onclike = slide(-1) will be fired when onload;
+  /*
+  prev.onclick = function(){slide(-1);};
+  next.onclick = function(){slide(1);};*/
+  prev.onclick = function () {
+    onClickPrev();
+  };
+  next.onclick = function () {
+    onClickNext();
+  };
+}
+
+function animate(opts) {
+  var start = new Date;
+  var id = setInterval(function () {
+    var timePassed = new Date - start;
+    var progress = timePassed / opts.duration;
+    if (progress > 1) {
+      progress = 1;
+    }
+    var delta = opts.delta(progress);
+    opts.step(delta);
+    if (progress == 1) {
+      clearInterval(id);
+      opts.callback();
+    }
+  }, opts.delay || 17);
+  //return id;
+}
+
+function slideTo(imageToGo) {
+  var direction;
+  var numOfImageToGo = Math.abs(imageToGo - currentImage);
+  // slide toward left
+
+  direction = currentImage > imageToGo ? 1 : -1;
+  currentPostion = -1 * currentImage * imageWidth;
+  var opts = {
+    duration: 500,
+    delta: function (p) {
+      return p;
+    },
+    step: function (delta) {
+      ul.style.left = parseInt(currentPostion + direction * delta * imageWidth * numOfImageToGo) + 'px';
+    },
+    callback: function () {
+      currentImage = imageToGo;
+    }
+  };
+  animate(opts);
+}
+
+function onClickPrev() {
+  if (currentImage == 0) {
+    slideTo(imageNumber - 1);
+  } else {
+    slideTo(currentImage - 1);
+  }
+}
+
+function onClickNext() {
+  if (currentImage == imageNumber - 1) {
+    slideTo(0);
+  } else {
+    slideTo(currentImage + 1);
+  }
+}
+
+window.onload = init;
+
+/* slider-2 */
